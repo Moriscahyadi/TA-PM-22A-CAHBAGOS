10c3433ce22116a9e9f1bf1dc6fc47c0b84179aa
FatkhurNurrohman <fatkhur_nurrohman@teknokrat.ac.id>
2024-06-11 22:47:11 +0700

    fatkhur:homeactifity

---
+++ PROGRAM/lumbung/app/src/main/java/com/uti/lumbung/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.uti.lumbung
+import android.R
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        // Inisialisasi dan logika untuk halaman utama
+    }
+}
+
