fec86c069fa01dbac8da25801d6343a834178f7c
FatkhurNurrohman <fatkhur_nurrohman@teknokrat.ac.id>
2024-06-17 22:23:37 +0700

    fatkhur: membuat variabel binding untuk LoginActivity

--- PROGRAM/lumbung/app/src/main/java/com/uti/lumbung/LoginActivity.kt
+++ PROGRAM/lumbung/app/src/main/java/com/uti/lumbung/LoginActivity.kt
@@ -7,11 +7,15 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.uti.lumbung.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var databaseHelper: dbHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
