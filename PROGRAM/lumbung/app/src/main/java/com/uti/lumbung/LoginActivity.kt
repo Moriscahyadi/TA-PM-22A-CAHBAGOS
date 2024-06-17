e5cdca0fa57bcbb938bce0ad524c91d3e1677fc6
FatkhurNurrohman <fatkhur_nurrohman@teknokrat.ac.id>
2024-06-17 22:21:50 +0700

    fatkhur: membuat deklarasi binding untuk databaseHelper

--- PROGRAM/lumbung/app/src/main/java/com/uti/lumbung/LoginActivity.kt
+++ PROGRAM/lumbung/app/src/main/java/com/uti/lumbung/LoginActivity.kt
@@ -9,37 +9,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var databaseHelper: dbHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)cd
-        val etUsername = findViewById<EditText>(R.id.etUsername)
-        val etPassword = findViewById<EditText>(R.id.etPassword)
-        val btnLogin = findViewById<Button>(R.id.btnLogin)
-        btnLogin.setOnClickListener {
-            val username = etUsername.getText().toString()
-            val password = etPassword.getText().toString()
-            if (validateLogin(username, password)) {
-                // Login berhasil, navigasi ke halaman berikutnya
-                val intent = Intent(
-                    this@LoginActivity,
-                    HomeActivity::class.java
-                )
-                startActivity(intent)
-                finish() // Selesai LoginActivity agar tidak bisa kembali dengan tombol back
-            } else {
-                // Login gagal, tampilkan pesan kesalahan
-                Toast.makeText(
-                    this@LoginActivity,
-                    "Login failed. Please check your credentials.",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-    }
-
-    private fun validateLogin(username: String, password: String): Boolean {
-        // Misalnya, kita melakukan validasi sederhana di sini.
-        // Anda dapat mengganti logika validasi sesuai dengan kebutuhan aplikasi Anda.
-        return username == "admin" && password == "admin123"
     }
 }
