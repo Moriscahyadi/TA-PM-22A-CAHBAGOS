3033a4117f591b4bcbaff2b8bb400318aaa54c58
FatkhurNurrohman <fatkhur_nurrohman@teknokrat.ac.id>
2024-06-17 22:07:23 +0700

    fatkhur: membuat table database

--- PROGRAM/lumbung/app/src/main/java/com/uti/lumbung/dbHelper.kt
+++ PROGRAM/lumbung/app/src/main/java/com/uti/lumbung/dbHelper.kt
@@ -1,6 +1,12 @@
 package com.uti.lumbung
 
-class dbHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class dbHelper(private val context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
     companion object {
         private const val DATABASE_NAME = "UserDatabase.db"
         private const val DATABASE_VERSION = 1
@@ -8,5 +14,17 @@ class dbHelper {
         private const val COLUMN_ID = "id"
         private const val COLUMN_USERNAME = "username"
         private const val COLUMN_PASSWORD = "password"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val createTableQuery = ("CREATE TABLE $TABLE_NAME (" +
+                "$COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "$COLUMN_USERNAME TEXT, " +
+                "$COLUMN_PASSWORD TEXT)")
+        db?.execSQL(createTableQuery)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
     }
 }
